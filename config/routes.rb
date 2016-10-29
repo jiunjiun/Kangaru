b7710aacc4b9085b88d35efd5d028dbe115ea54b
jiunjiun <quietmes@gmail.com>
2016-10-29 15:50:08 +0800

    Add - Add Home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  root 'home#index'
+
+  namespace :admin do
+    get :dashboard, to: 'dashboard#index', as: :dashboard
+
+    resource :adapters
+  end
 end
